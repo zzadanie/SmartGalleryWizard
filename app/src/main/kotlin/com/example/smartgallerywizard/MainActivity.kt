9b9a2fd17adf36c4e8547868698fd9503aabdf35
Adrian <220956@student.pwr.edu.pl>
2019-03-30 15:51:16 +0100

    app compat layout

--- app/src/main/kotlin/com/example/smartgallerywizard/MainActivity.kt
+++ app/src/main/kotlin/com/example/smartgallerywizard/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.smartgallerywizard
 
-import android.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -8,8 +8,6 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.View.*
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_fullscreen.*
